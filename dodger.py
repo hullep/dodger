b08133ecced8a7f9a39b71957cb27b8a32030cad
hullep <hullep_20@yahoo.com>
2021-12-21 02:22:09 -0500

    Add enemies and update images
    Enemies are generated at consistent time intervals but at random speeds moving from right to left. The player and enemy icons were updated to custom images. Game exists when a player contacts an enemy.

--- dodger.py
+++ dodger.py
@@ -12,12 +12,16 @@ Created on Mon Dec 20 13:54:57 2021
 
 import pygame
 
+# Import random module
+import random
+
 
 # Import pygame.locals for easier access to key coordinates
 
 # Updated to conform to flake8 and black standards
 
 from pygame.locals import (
+    RLEACCEL,
 
     K_UP,
 
@@ -53,9 +57,9 @@ class Player(pygame.sprite.Sprite):
 
         super(Player, self).__init__()
 
-        self.surf = pygame.Surface((75, 25))
+        self.surf = pygame.image.load("bird.png")
 
-        self.surf.fill((255, 255, 255))
+        self.surf.set_colorkey((255, 255, 255), RLEACCEL)
 
         self.rect = self.surf.get_rect()
 
@@ -63,22 +67,63 @@ class Player(pygame.sprite.Sprite):
 
     def update(self, pressed_keys):
         if pressed_keys[K_UP]:
-            self.rect.move_ip(0, -5)
+            self.rect.move_ip(0, -1)
         if pressed_keys[K_DOWN]:
-            self.rect.move_ip(0, 5)
+            self.rect.move_ip(0, 1)
         if pressed_keys[K_LEFT]:
-            self.rect.move_ip(-5, 0)
+            self.rect.move_ip(-1, 0)
         if pressed_keys[K_RIGHT]:
-            self.rect.move_ip(5, 0)
+            self.rect.move_ip(1, 0)
 
         if self.rect.left < 0:
             self.rect.left = 0
-        if self.rect. right > SCREEN_WIDTH
+        if self.rect. right > SCREEN_WIDTH:
             self.rect.right = SCREEN_WIDTH
         if self.rect.top <= 0:
             self.rect.top = 0
         if self.rect.bottom >= SCREEN_HEIGHT:
             self.rect.bottom = SCREEN_HEIGHT
+
+# Define the enemy object by extending pygame.sprite.Sprite
+
+# The surface you draw on the screen is now an attribute of 'enemy'
+
+class Enemy(pygame.sprite.Sprite):
+
+    def __init__(self):
+
+        super(Enemy, self).__init__()
+
+        self.surf = pygame.image.load("plane.png")
+
+        self.surf.set_colorkey((255,255,255), RLEACCEL)
+
+        self.rect = self.surf.get_rect(
+
+            center=(
+
+                random.randint(SCREEN_WIDTH + 20, SCREEN_WIDTH + 100),
+
+                random.randint(0, SCREEN_HEIGHT),
+
+            )
+
+        )
+
+        self.speed = random.randint(1, 3)
+
+
+    # Move the sprite based on speed
+
+    # Remove the sprite when it passes the left edge of the screen
+
+    def update(self):
+
+        self.rect.move_ip(-self.speed, 0)
+
+        if self.rect.right < 0:
+
+            self.kill()
 
 # Initialize pygame
 
@@ -91,10 +136,29 @@ pygame.init()
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+# Create a custom event for adding a new enemy
+
+ADDENEMY = pygame.USEREVENT + 1
+
+pygame.time.set_timer(ADDENEMY, 550)
+
 
 # Instantiate player. Right now, this is just a rectangle.
 
 player = Player()
+
+# Create groups to hold enemy sprites and all sprites
+
+# - enemies is used for collision detection and position updates
+
+# - all_sprites is used for rendering
+
+enemies = pygame.sprite.Group()
+
+all_sprites = pygame.sprite.Group()
+
+all_sprites.add(player)
+
 
 
 # Variable to keep the main loop running
@@ -125,10 +189,29 @@ while running:
         elif event.type == QUIT:
 
             running = False
+            
+        # Add a new enemy?
+
+        elif event.type == ADDENEMY:
+
+            # Create the new enemy and add it to sprite groups
+
+            new_enemy = Enemy()
+
+            enemies.add(new_enemy)
+
+            all_sprites.add(new_enemy)
+
+    # Get the set of keys pressed and check for user input
 
     pressed_keys = pygame.key.get_pressed()
 
     player.update(pressed_keys)
+
+    # Update enemy position
+
+    enemies.update()
+
 
 
     # Fill the screen with black
@@ -136,9 +219,21 @@ while running:
     screen.fill((0, 0, 0))
 
 
-    # Draw the player on the screen
+    # Draw all sprites
 
-    screen.blit(player.surf, player.rect)
+    for entity in all_sprites:
+
+        screen.blit(entity.surf, entity.rect)
+
+    # Check if any enemies have collided with the player
+
+    if pygame.sprite.spritecollideany(player, enemies):
+
+        # If so, then remove the player and stop the loop
+
+        player.kill()
+
+        running = False
 
 
     # Update the display
